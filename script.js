9cadc430c2753023a0d9873ad8fc85c29deeaafa
Chris_auty <mmf2003@ukr.net>
2023-03-24 17:42:58 +0200

    Chapter II Eloquent JavaScript

--- script.js
+++ script.js
@@ -1634,3 +1634,38 @@ console.log(meeting("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Met
 
 ///////////////////////////////////////////////////////////////////////////////////
 
+//Chapter II Eloquent JavaScript
+//Exersize 1
+
+/* for (let i = '#'; i.length <= 7; i += '#') {
+    console.log(i);
+}
+
+//Exersize 2
+
+for (let i = 1; i <= 100; i++) {
+    let res = '';
+    if ( i % 3 == 0) res += 'Fizz';
+    if ( i % 5 == 0) res += 'Buzz';
+    console.log(res || i);
+}
+
+//Exersize 3
+
+let size = 8;
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board); */
+
+///////////////////////////////////////////////////////////////////////////////////
